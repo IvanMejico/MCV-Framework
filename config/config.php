c3eec23817bb194f4e22939bd193436a83b68f65
IvanMejico <ivan.mejico21@gmail.com>
2019-06-01 11:00:22 +0800

    initital commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DEBUG', true);
+
+define('DEFAULT_CONTROLLER', 'Home'); //  default_controller if there isn't one defind in the url
+define('DEFAULT_LAYOUT', 'default'); // if no layout is set in the controller use this layout.
+
+DEFINE('PROOT', '/ruah/'); // set this to '/' for a live server.
+
+define('SITE_TITLE', 'Ruah MVC Framework'); // This will be used if no site title is SITE_TITLE
